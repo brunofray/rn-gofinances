6c63ab4bb1da30bfeba8b0ec36e6ae860d337b3b
Bruno Fray <brunofray@gmail.com>
2022-06-06 09:57:20 -0300

    Correção StatusBar - Android

--- App.tsx
+++ App.tsx
@@ -32,7 +32,7 @@ export default function App() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <StatusBar barStyle="light-content"/>
+          <StatusBar barStyle="light-content" translucent={true} backgroundColor={theme.colors.primary} />
           <AppRoutes />
         </NavigationContainer>
       </ThemeProvider>
