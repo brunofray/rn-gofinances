a62ec0014f24d6c9d2375843b6edb8cbf379bb96
Bruno Fray <brunofray@gmail.com>
2022-06-06 20:49:42 -0300

    Conectando com uma conta Google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, ReactNode, useContext } from 'react';
+import React, { createContext, ReactNode, useContext, useState } from 'react';
+
+import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -8,24 +10,60 @@ interface User {
   id: string;
   name: string;
   email: string;
-  phone?: string;
+  photo?: string;
 }
 
 interface AuthContextData {
   user: User;
+  signInWithGoogle(): Promise<void>;
+}
+
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  },
+  type: string;
 }
 
 const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps){
-  const user = {
-    id: '12421321',
-    name: 'Bruno Fray',
-    email: 'brunofray@gmail.com',
-  };
+  const [user, setUser] = useState<User>({} as User);
+
+  async function signInWithGoogle() {
+    try {
+      const CLIENT_ID = '931822272770-ndp1vfq5orim6i8dkdebi9aktrq8f779.apps.googleusercontent.com';
+      const REDIRECT_URI = 'https://auth.expo.io/@brunofray/gofinances';
+      const RESPONSE_TYPE = 'token';
+      const SCOPE = encodeURI('profile email');
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const { type, params } = await AuthSession
+      .startAsync({ authUrl }) as AuthorizationResponse;
+
+      if ( type === 'success' ) {
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        const userInfo = await response.json();
+
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture,
+        });
+      }
+
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
 
   return(
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ 
+      user,
+      signInWithGoogle
+    }}>
       { children }
     </AuthContext.Provider>
   );
