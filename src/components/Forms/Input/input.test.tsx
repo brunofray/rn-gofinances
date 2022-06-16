3c4b1b7cb7335ce1aac37f70ccb8e82f7b2f3902
Bruno Fray <brunofray@gmail.com>
2022-06-16 17:20:16 -0300

    Teste em Componentes

---
+++ src/components/Forms/Input/input.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react-native';
+import React from 'react';
+
+import { Input } from '.';
+
+describe('Input Component', () => {
+  it('must have specific border color when active', () => {
+    const { getByTestId } = render(
+      <Input
+        testID="input-email"
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCorrect={false}
+        active={true}
+      />
+    );
+
+    const inputComponent = getByTestId('input-email');
+    expect(inputComponent.props.style[0].borderColor).toEqual('#E83F5B');
+  });
+});
