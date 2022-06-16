6e085293866671e3e29ed06b543e765cde8448c7
Bruno Fray <brunofray@gmail.com>
2022-06-16 18:12:56 -0300

    Testes com Contexto

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "/android",
     "/ios"
   ],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+  ],
   setupFilesAfterEnv: [
-    "@testing-library/jest-native/extend-expect"
+    "@testing-library/jest-native/extend-expect",
+    "jest-styled-components"
   ]
 }
--- src/components/Forms/Input/input.test.tsx
+++ src/components/Forms/Input/input.test.tsx
@@ -2,6 +2,16 @@ import { render } from '@testing-library/react-native';
 import React from 'react';
 
 import { Input } from '.';
+import { ThemeProvider } from 'styled-components/native';
+import theme from '../../../global/styles/theme';
+
+const Providers: React.FC = ({ children }: any) => {
+  return (
+    <ThemeProvider theme={theme}>
+      { children }
+    </ThemeProvider>
+  );
+};
 
 describe('Input Component', () => {
   it('must have specific border color when active', () => {
@@ -12,7 +22,10 @@ describe('Input Component', () => {
         keyboardType="email-address"
         autoCorrect={false}
         active={true}
-      />
+      />,
+      {
+        wrapper: Providers
+      }
     );
 
     const inputComponent = getByTestId('input-email');
