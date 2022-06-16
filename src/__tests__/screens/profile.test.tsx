220e812d46feb1b601ba225f98e27908008d4d17
Bruno Fray <brunofray@gmail.com>
2022-06-16 16:33:27 -0300

    Seletores

--- src/__tests__/screens/profile.test.tsx
+++ src/__tests__/screens/profile.test.tsx
@@ -10,3 +10,21 @@ test('check if show correctly user input name placeholder', () => {
   
   expect(inputName).toBeTruthy();
 });
+
+test('check if user data has been loaded', () => {
+  const { getByTestId } = render(<Profile />);
+
+  const inputName = getByTestId('input-name');
+  const inputSurname = getByTestId('input-surname');
+
+  expect(inputName.props.value).toEqual('Bruno');
+  expect(inputSurname.props.value).toEqual('Fray');
+});
+
+test('check if title render correctly', () => {
+  const { getByTestId } = render(<Profile />);
+
+  const textTitle = getByTestId('text-title');
+
+  expect(textTitle.props.children).toContain('Perfil');
+});
